4d7432ae339357d4ddc1763c30b499c5f0ddf10f
simfody <ymilapino.gpay@gmail.com>
2024-03-15 12:47:45 +0300

    Initial commit

---
+++ src/test/java/ru/simfody/webscraper/WebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.simfody.webscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
