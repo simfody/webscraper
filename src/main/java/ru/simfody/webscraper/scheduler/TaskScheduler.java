74154d999a574af489d5e98ad7b9fd7a37564ee5
simfody <ymilapino.gpay@gmail.com>
2024-05-28 15:07:19 +0300

    implementation 'net.sourceforge.htmlunit:htmlunit:2.70.0'

--- src/main/java/ru/simfody/webscraper/scheduler/TaskScheduler.java
+++ src/main/java/ru/simfody/webscraper/scheduler/TaskScheduler.java
@@ -2,12 +2,19 @@ package ru.simfody.webscraper.scheduler;
 
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
+import ru.simfody.webscraper.detail.ScraperTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // 28.05.2024
 @Component("taskScheduler")
 public class TaskScheduler {
-    // Запуск каждые 900 сек. (15 мин)
-//		@Scheduled(cron="0/900 * * * * *")
-    public void scraperTaskRun() {
+    private List<ScraperTask> scraperTaskList = new ArrayList<>();
+    private boolean           busy = false;
+    // Запуск каждые 60 сек.
+    @Scheduled(fixedRate = 60000)
+    public void scraperTaskQueueHandlerRun() {
 
     }
 }
