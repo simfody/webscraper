958eb3f9c875c88b355ba2a8384876343e76573b
simfody <ymilapino.gpay@gmail.com>
2024-06-03 14:47:22 +0300

    Add entity tables

---
+++ src/main/java/ru/simfody/webscraper/entity/Region.java
@@ -0,0 +1,14 @@
+package ru.simfody.webscraper.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// 03.06.2024
+@Entity
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
