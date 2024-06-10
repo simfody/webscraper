a795eae36ebced5ec946a70f01eba7c4a6b3d795
simfody <ymilapino.gpay@gmail.com>
2024-06-10 12:30:14 +0300

    rename DataStore

--- src/main/java/ru/simfody/webscraper/entity/DataStore.java
+++ src/main/java/ru/simfody/webscraper/entity/DataStore.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 
 // 03.06.2024
 @Entity
-public class CollectedDataStore {
+public class DataStore {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
