ff025b05575f3a696e0636b89383c0b8fe45e0dc
simfody <ymilapino.gpay@gmail.com>
2024-06-10 16:21:38 +0300

    add fields in FieldLocation

--- src/main/java/ru/simfody/webscraper/entity/FieldLocation.java
+++ src/main/java/ru/simfody/webscraper/entity/FieldLocation.java
@@ -1,11 +1,20 @@
 package ru.simfody.webscraper.entity;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 // 07.06.2024
+@Entity
 public class FieldLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
     private ContentPage contentPageId;
     @ManyToOne
     private FieldContentType fieldContentTypeId;
+    @Column(length = 25, nullable = false)
+    private String name;
+    @Column(length = 255, nullable = false)
+    private String locationStr;
 
 }
