17f8c58b8916371384417f98008aac4a4cf075b2
simfody <ymilapino.gpay@gmail.com>
2024-06-13 16:46:36 +0300

    add fields to tbl ParseScheduler 2

--- src/main/java/ru/simfody/webscraper/entity/ParseScheduler.java
+++ src/main/java/ru/simfody/webscraper/entity/ParseScheduler.java
@@ -8,11 +8,21 @@ public class ParseScheduler {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int second = - 1;             //(0-59) '-1' - is off
-    private int minute = - 1;             // (0 - 59) '-1' - is off
-    private int hour = - 1;               // (0 - 23) '-1' - is off
-    private int dayOfTheMonth = - 1;      //  (1 - 31) '-1' - is off
-    private int month = - 1;              //   (1 - 12) (or JAN-DEC) '-1' - is off
+    /*
+    schedule Cron expression
+     ┌───────────── second (0-59)
+     │ ┌───────────── minute (0 - 59)
+     │ │ ┌───────────── hour (0 - 23)
+     │ │ │ ┌───────────── day of the month (1 - 31)
+     │ │ │ │ ┌───────────── month (1 - 12) (or JAN-DEC)
+     │ │ │ │ │ ┌───────────── day of the week (0 - 7)
+     │ │ │ │ │ │          (0 or 7 is Sunday, or MON-SUN)
+    */
+    private int second = - 1;             //(0-59) <- '-1' - is "*"
+    private int minute = - 1;             // (0 - 59) <- '-1' - is "*"
+    private int hour = - 1;               // (0 - 23) <- '-1' - is "*"
+    private int dayOfTheMonth = - 1;      //  (1 - 31) <- '-1' - is "*"
+    private int month = - 1;              //   (1 - 12) (or JAN-DEC)  <- '-1' - is "*"
     private int dayOfTheWeek = - 1;       //   dayOfTheWeek (1 - 7 -> MON-SUN)
     @ManyToOne
     private ParseSite siteForParseParamId;
