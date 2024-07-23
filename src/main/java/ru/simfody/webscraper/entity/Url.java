7db76f2ca18270a6110a7ef3cbc890042a897cc6
simfody <ymilapino.gpay@gmail.com>
2024-07-23 15:01:02 +0300

    remade ActionScript

--- src/main/java/ru/simfody/webscraper/entity/Url.java
+++ src/main/java/ru/simfody/webscraper/entity/Url.java
@@ -8,10 +8,8 @@ public class Url {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 25, nullable = false)
-    private String name;
-    @ManyToOne
-    private Client clientId;
+    @Column(length = 255, nullable = false)
+    private String urlStr;
     @ManyToOne
     private ParsingObject parsingObjectId;
 
